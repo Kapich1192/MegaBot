341ad0562f09792f7b44ac6d78a78b1d3dfa6e50
barbreye <barbreye@student.21-school.ru>
2023-01-21 11:40:21 +0300

    reliase 1.0

--- src/main/java/com/MegaBot/service/Answer.java
+++ src/main/java/com/MegaBot/service/Answer.java
@@ -7,14 +7,15 @@ import java.util.Scanner;
 
 
 public class Answer {
-    String promo;
-    ArrayList<String> honor = new ArrayList<>();
-    ArrayList<String> apple = new ArrayList<>();
-    ArrayList<String> samsung = new ArrayList<>();
-    ArrayList<String> vivo = new ArrayList<>();
-    ArrayList<String> xiaomi = new ArrayList<>();
-    ArrayList<String> poco = new ArrayList<>();
-    ArrayList<String> tecno = new ArrayList<>();
+    private String promo;
+    private ArrayList<String> honor = new ArrayList<>();
+    private ArrayList<String> apple = new ArrayList<>();
+    private ArrayList<String> samsung = new ArrayList<>();
+    private ArrayList<String> vivo = new ArrayList<>();
+    private ArrayList<String> xiaomi = new ArrayList<>();
+    private ArrayList<String> poco = new ArrayList<>();
+    private ArrayList<String> tecno = new ArrayList<>();
+    private ArrayList<String> realme = new ArrayList<>();
 
     public Answer() throws IOException{
         refreshPromo();
@@ -23,20 +24,120 @@ public class Answer {
         return "honor: " + honor.size() + ";\n" +
                 "apple: " + apple.size() + ";\n" +
                 "samsung: " + samsung.size() + ";\n" +
-                "vivo: " + honor.size() + ";\n" +
-                "xiaomi: " + honor.size() + ";\n" +
-                "poco: " + honor.size() + ";\n" +
-                "tecno: " + honor.size() + ";\n"
+                "vivo: " + vivo.size() + ";\n" +
+                "xiaomi: " + xiaomi.size() + ";\n" +
+                "poco: " + poco.size() + ";\n" +
+                "tecno: " + tecno.size() + ";\n" +
+                "realme: " + realme.size() + ";\n"
                 ;
     }
     public void refreshPromo() {
+        honor.clear();
+        apple.clear();
+        samsung.clear();
+        vivo.clear();
+        xiaomi.clear();
+        poco.clear();
+        tecno.clear();
+        realme.clear();
+
         File file = new File("promo/honor.txt");
         try {
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 String i = sc.nextLine();
                 honor.add(i);
-                System.out.println(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/apple.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                apple.add(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/realme.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                realme.add(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/tecno.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                tecno.add(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/poco.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                poco.add(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/samsung.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                samsung.add(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/vivo.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                vivo.add(i);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file = new File("promo/xiaomi.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String i = sc.nextLine();
+                xiaomi.add(i);
             }
             sc.close();
         }
@@ -46,14 +147,219 @@ public class Answer {
     }
 
     public String nextPromo(String key) {
-        if(key.equals("honor") && honor.size() > 0){
-            promo = "honor - " + honor.get(0);
-            honor.remove(0);
-        } else {
-            promo = "Промики на Honor закончились(((";
+        if(key.equals("honor")) {
+            if (honor.size() > 0) {
+                promo = "honor - " + honor.get(0);
+                honor.remove(0);
+            } else {
+                promo = "Промики на Honor закончились(((";
+            }
+        } else if (key.equals("apple")) {
+            if (apple.size() > 0) {
+                promo = "apple - " + apple.get(0);
+                apple.remove(0);
+            } else {
+                promo = "Промики на Apple закончились(((";
+            }
+        } else if(key.equals("samsung")) {
+            if (samsung.size() > 0) {
+                promo = "samsung - " + samsung.get(0);
+                samsung.remove(0);
+            } else {
+                promo = "Промики на Samsung закончились(((";
+            }
+        } else if (key.equals("realme") ) {
+            if ( realme.size() > 0) {
+                promo = "realme - " + realme.get(0);
+                realme.remove(0);
+            } else {
+                promo = "Промики на Realme закончились(((";
+            }
+        } else if (key.equals("xiaomi") ) {
+            if ( xiaomi.size() > 0) {
+                promo = "xiaomi - " + xiaomi.get(0);
+                xiaomi.remove(0);
+            } else {
+                promo = "Промики на Xiaomi закончились(((";
+            }
+        } else if (key.equals("poco") ) {
+            if (poco.size() > 0) {
+                promo = "poco - " + poco.get(0);
+                poco.remove(0);
+            } else {
+                promo = "Промики на Poco закончились(((";
+            }
+        } else if (key.equals("vivo")) {
+            if (vivo.size() > 0) {
+                promo = "vivo - " + vivo.get(0);
+                vivo.remove(0);
+            } else {
+                promo = "Промики на Vivo закончились(((";
+            }
+        } if (key.equals("tecno")) {
+            if ( tecno.size() > 0) {
+                promo = "tecno - " + tecno.get(0);
+                tecno.remove(0);
+            } else {
+                promo = "Промики на Tecno закончились(((";
+            }
         }
         return promo;
     }
+/* Действия с промиками Honor */
+    public void addPromoHonor(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                honor.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/honor.txt"));
+            for (int i = 0; i < honor.size(); i++) {
+                writer.write(honor.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
+    /* Действия с промиками Apple */
+    public void addPromoApple(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                apple.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/apple.txt"));
+            for (int i = 0; i < apple.size(); i++) {
+                writer.write(apple.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
 
+    /* Действия с промиками Realme */
+    public void addPromoRealme(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                realme.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/realme.txt"));
+            for (int i = 0; i < realme.size(); i++) {
+                writer.write(realme.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
 
+    /* Действия с промиками Samsung */
+    public void addPromoSamsung(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                samsung.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/samsung.txt"));
+            for (int i = 0; i < samsung.size(); i++) {
+                writer.write(samsung.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
+    /* Действия с промиками Xiaomi */
+    public void addPromoXiaomi(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+               xiaomi.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/xiaomi.txt"));
+            for (int i = 0; i < xiaomi.size(); i++) {
+                writer.write(xiaomi.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
+    /* Действия с промиками Poco */
+    public void addPromoPoco(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                poco.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/poco.txt"));
+            for (int i = 0; i < poco.size(); i++) {
+                writer.write(poco.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
+
+    /* Действия с промиками Honor */
+    public void addPromoVivo(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                vivo.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/vivo.txt"));
+            for (int i = 0; i < vivo.size(); i++) {
+                writer.write(vivo.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
+
+    /* Действия с промиками Tecno */
+    public void addPromoTecno(String key) {
+        String[] list = key.split(" ");
+        if(list.length > 2) {
+            for (int i = 2; i < list.length; i++) {
+                tecno.add(list[i]);
+            }
+        }
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("promo/tecno.txt"));
+            for (int i = 0; i < tecno.size(); i++) {
+                writer.write(tecno.get(i) + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.refreshPromo();
+    }
 }
--- src/main/java/com/MegaBot/service/TelBot.java
+++ src/main/java/com/MegaBot/service/TelBot.java
@@ -33,85 +33,105 @@ public class TelBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chartId = update.getMessage().getChatId();
-            switch (messageText) {
-                case "/start" :
-                    try {
-                        startCommandReceived(chartId, update.getMessage().getChat().getFirstName());
-                    } catch (TelegramApiException e){
-                        throw new RuntimeException(e);
-                    }
-                    break;
-                //  1. realme
-                case "realme":
-                        sendMessage(chartId,"Хочешь наебать черного брата?");
-                    break;
-                //  2. apple
-                case "apple":
-                    sendMessage(chartId,"А не дохуя ли ?");
-                    break;
-                //  3. samsung
-                case "samsung":
-                    sendMessage(chartId,"тыщенку в кассу?");
-                    break;
-                //  4. xiaomi
-                case "xiaomi":
-                    sendMessage(chartId,"опять опаздал чтоли?");
-                    break;
-                //  6. honor
-                case "honor":
 
-                    sendMessage(chartId,answer.nextPromo("honor"));
-                    break;
-                //  7. vivo
-                case "vivo":
-                    sendMessage(chartId,"Вот пиздюк!");
-                    break;
-                //  8. poco
-                case "poco":
-                    sendMessage(chartId,"Вот пиздюк!");
-                    break;
-                //  9. realme
-                //  10. realme
-                //  11. realme
-                //  12. realme
-                //  13. realme
-                //  14. realme
-                //  15. refresh
-                case "refresh":
-                    answer.refreshPromo();
-                    sendMessage(chartId,"Обновление списка промокодов!!!");
-                    break;
-                //  16. count
-                case "count":
-                    sendMessage(chartId,answer.getCount());
-                    break;
-                //  17. realme
-                //  18. realme
-                //  19. realme
-                //  20. realme
-                //  21. realme
-                case "info":
-                    String info = "Основные команды для Бота: \n" +
-                            "   1. realme - высылает промик на realme;\n" +
-                            "   2. apple - высылает промик на apple;\n" +
-                            "   3. samsung - высылает промик на samsung;\n" +
-                            "   4. xiaomi - высылает промик на xiaomi;\n" +
-                            "   5. honor - высылает промик на xiaomi;\n" +
-                            "   6. vivo - высылает промик на xiaomi;\n" +
-                            "   7. poco - высылает промик на xiaomi;\n" +
-                            "   8. add realme XXXXXXXXXX - добавляет промокод в стек realme;\n" +
-                            "   9. add apple XXXXXXXXXX - добавляет промокод в стек apple;\n" +
-                            "   10. add samsung XXXXXXXXXX - добавляет промокод в стек samsung;\n" +
-                            "   11. add xiaomi XXXXXXXXXX - добавляет промокод в стек xiaomi;\n" +
-                            "   12. add honor XXXXXXXXXX - добавляет промокод в стек honor;\n" +
-                            "   13. add vivo XXXXXXXXXX - добавляет промокод в стек vivo;\n" +
-                            "   14. add poco XXXXXXXXXX - добавляет промокод в стек poco;\n" +
-                            "   15. refresh - обновляет списки промокодов;\n " +
-                            "   16. count - выводит остатки промокодов;\n "
-                            ;
-                    sendMessage(chartId,info);
-                    break;
+            if (messageText.contains("add honor")) {
+                answer.addPromoHonor(messageText);
+                sendMessage(chartId,"Промики на Хонор добавлены!");
+            } else if (messageText.contains("add apple")) {
+                answer.addPromoApple(messageText);
+                sendMessage(chartId,"Промики на Apple добавлены!");
+            } else if (messageText.contains("add samsung")) {
+                answer.addPromoSamsung(messageText);
+                sendMessage(chartId,"Промики на Samsung добавлены!");
+            } else if (messageText.contains("add realme")) {
+                answer.addPromoRealme(messageText);
+                sendMessage(chartId,"Промики на Realme добавлены!");
+            } else if (messageText.contains("add xiaomi")) {
+                answer.addPromoXiaomi(messageText);
+                sendMessage(chartId,"Промики на Xiaomi добавлены!");
+            } else if (messageText.contains("add tecno")) {
+                answer.addPromoTecno(messageText);
+                sendMessage(chartId,"Промики на Tecno добавлены!");
+            } else if (messageText.contains("add vivo")) {
+                answer.addPromoVivo(messageText);
+                sendMessage(chartId,"Промики на Vivo добавлены!");
+            } else if (messageText.contains("add poco")) {
+                answer.addPromoPoco(messageText);
+                sendMessage(chartId,"Промики на Poco добавлены!");
+            } else {
 
+                switch (messageText) {
+                    case "/start":
+                        try {
+                            startCommandReceived(chartId, update.getMessage().getChat().getFirstName());
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                        break;
+                    //  1. realme
+                    case "realme":
+                        sendMessage(chartId, answer.nextPromo("realme"));
+                        break;
+                    //  2. apple
+                    case "apple":
+                        sendMessage(chartId, answer.nextPromo("apple"));
+                        break;
+                    //  3. samsung
+                    case "samsung":
+                        sendMessage(chartId, answer.nextPromo("samsung"));
+                        break;
+                    //  4. xiaomi
+                    case "xiaomi":
+                        sendMessage(chartId, answer.nextPromo("xiaomi"));
+                        break;
+                    //  6. honor
+                    case "honor":
+                        sendMessage(chartId, answer.nextPromo("honor"));
+                        break;
+                    //  7. vivo
+                    case "vivo":
+                        sendMessage(chartId, answer.nextPromo("vivo"));
+                        break;
+                    //  8. poco
+                    case "poco":
+                        sendMessage(chartId, answer.nextPromo("poco"));
+                        break;
+                    //  9. tecno
+                    case "tecno":
+                        sendMessage(chartId, answer.nextPromo("tecno"));
+                        break;
+                    //  10. refresh
+                    case "refresh":
+                        answer.refreshPromo();
+                        sendMessage(chartId, "Обновление списка промокодов!!!");
+                        break;
+                    //  11. count
+                    case "count":
+                        sendMessage(chartId, answer.getCount());
+                        break;
+                    //  12. Info
+                    case "info":
+                        String info = "Основные команды для Бота: \n" +
+                                "   1. realme - высылает промик на realme;\n" +
+                                "   2. apple - высылает промик на apple;\n" +
+                                "   3. samsung - высылает промик на samsung;\n" +
+                                "   4. xiaomi - высылает промик на xiaomi;\n" +
+                                "   5. honor - высылает промик на xiaomi;\n" +
+                                "   6. vivo - высылает промик на xiaomi;\n" +
+                                "   7. poco - высылает промик на xiaomi;\n" +
+                                "   8. add realme XXXXXXXXXX - добавляет промокод в стек realme;\n" +
+                                "   9. add apple XXXXXXXXXX - добавляет промокод в стек apple;\n" +
+                                "   10. add samsung XXXXXXXXXX - добавляет промокод в стек samsung;\n" +
+                                "   11. add xiaomi XXXXXXXXXX - добавляет промокод в стек xiaomi;\n" +
+                                "   12. add honor XXXXXXXXXX - добавляет промокод в стек honor;\n" +
+                                "   13. add vivo XXXXXXXXXX - добавляет промокод в стек vivo;\n" +
+                                "   14. add poco XXXXXXXXXX - добавляет промокод в стек poco;\n" +
+                                "   15. refresh - обновляет списки промокодов;\n " +
+                                "   16. count - выводит остатки промокодов;\n ";
+                        sendMessage(chartId, info);
+                        break;
+
+                }
             }
         }
     }
